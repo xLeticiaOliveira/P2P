9203b9c6596a6ea3d04ee806a1af53b006d32f49
Marcelo Jr <laendle1999@gmail.com>
2019-10-29 19:23:58 -0300

    Criação do arquivo de rotas

--- p2p/lib/main.dart
+++ p2p/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:p2p/RouteGenerator.dart';
 import 'package:p2p/pages/login/Login.dart';
 
 void main() {
-  runApp(new MaterialApp(home: new Login()));
+  runApp(new MaterialApp(
+    home: new Login(),
+    initialRoute: '/',
+    onGenerateRoute: RouteGenerator.generateRoute,
+  ));
 }
